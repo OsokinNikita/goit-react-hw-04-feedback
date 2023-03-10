57bcfb95a711111a081f8fa9ddae04e1c6e07b4c
OsokinNikita <110087045+OsokinNikita@users.noreply.github.com>
2023-03-10 10:53:28 +0200

    up for lesons 4

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,23 +2,26 @@ import PropTypes from 'prop-types';
 import { Box } from '../Box';
 import { Text } from './Statistics.styled';
 
-export const Statistics = ({ positivePercentage, ...props }) => {
+export const Statistics = ({ positivePercentage, total, options }) => {
   return (
     <Box p={2} display="flex" flexDirection="column">
-      {Object.keys(props).map(option => (
+      {Object.keys(options).map(option => (
         <Text key={option}>
-          {option}: {props[option]}
+          {option}: {options[option]}
         </Text>
       ))}
+      <Text>Total: {total}</Text>
       <Text>Positive feedback: {positivePercentage}%</Text>
     </Box>
   );
 };
 
 Statistics.propTypes = {
-  good: PropTypes.string,
-  neutral: PropTypes.string,
-  bad: PropTypes.string,
+  options: PropTypes.shape({
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+  }),
   total: PropTypes.number,
   positivePercentage: PropTypes.number,
 };
